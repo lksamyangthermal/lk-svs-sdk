cb0eab0dd630b34e271828d80bf24b3341d930ec
YOONYEOSEONG\SYOPT <fizzycrisp@syopt.co.kr>
2024-05-07 17:55:21 +0900

    Camera 연결 확인 UI 활성화 (Broadcast 필요)

--- WpfSample/ViewModel/PreviewViewModel.cs
+++ WpfSample/ViewModel/PreviewViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using KIRSharp;
+using WpfSample.Model;
 using WpfSample.Model.Handler;
 
 namespace WpfSample.ViewModel
@@ -13,6 +17,29 @@ namespace WpfSample.ViewModel
         [ObservableProperty]
         private DeviceHandler _deviceHandler = DeviceHandler.Instance;
 
+        [ObservableProperty]
+        private List<PseudoColor.Type> _pseudoColors = Enum.GetValues(typeof(PseudoColor.Type)).Cast<PseudoColor.Type>().ToList();
+
         public PreviewViewModel() { }
+
+        [RelayCommand]
+        private async Task Disconnect()
+        {
+            await DeviceHandler.SelectedDevice.DisposeAsync();
+        }
+
+        [RelayCommand]
+        private async Task ActiveOnceShutter()
+        {
+            try
+            {
+                var udpCamera = (UdpCamera)DeviceHandler.SelectedDevice.Camera;
+                await udpCamera.RunShutterManually();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"ActiveOnceShutter: {ex.Message}");
+            }
+        }
     }
 }
